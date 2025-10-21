d4e8ad6a5cf4118586d52185ff21be19f48e49ac
Hanbanan03 <hannah.watkin@ucas-edu.net>
2025-10-21 14:41:48 -0600

    did stuff
    Co-authored-by: DaddyMqnkey72 <DaddyMqnkey72@users.noreply.github.com>
    Co-authored-by: archie-buss <archie-buss@users.noreply.github.com>
    Co-authored-by: fastslow712 <fastslow712@users.noreply.github.com>

--- csp_hangman.c
+++ csp_hangman.c
@@ -1,1 +1,30 @@
-//IL,Ak,AH.HW 7th
+//IL,Ak,AH.HW 7th hangman final
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+//Archie  
+//introduction to game
+
+int main(void){
+    
+    char name[20];
+    printf("Welcome to the Halloween hangman game! Well, come play the game.\n");
+    printf("What is your name? ");
+    if (scanf("%99s", name) != 1) name[0]  '\0';
+    printf("hello, %s\n", name);
+    
+    srand(time(NULL));
+    char words[][10] = "boo", "cat", "ghost", "costume", "knight", "candy", "decay", "sweets", "screams" 
+
+
+//
+
+
+    return 0;
+}
+
+
+
+
+
