1df627203571f7951cd8c2f596a5572218386d18
Hanbanan03 <hannah.watkins@ucas-edu.net>
2025-10-25 00:00:38 +0000

    finished hangman

--- csp_hangman.c
+++ csp_hangman.c
@@ -1,8 +1,10 @@
-//IL,Ak,AH.HW 7th hangman final
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
+//Hannah
+//Game board
 void displayHangman(int wrong) {
     if (wrong == 0) {
         printf("\n  _______\n");
@@ -67,59 +69,84 @@ void displayHangman(int wrong) {
         printf(" |\n");
         printf("_|_   GAME OVER!\n");
     }
-//Archie  
-//introduction to game
+}
 
-int main(void){
-    srand(time(NULL));
+int main() {
+    srand((unsigned)time(NULL));
+
     char name[20];
-    printf("Welcome to the Halloween hangman game! Well, come play the game.\n");
+    char words[][20] = {
+        "boo","blackcat","ghost","costume","knight","candy","decay",
+        "sweets","screams","pumpkin","bats","mummy","snake","haunted",
+        "werewolf","dracula","witch","scary"
+    };
+
+    char board[50];
+    char wrongLetters[50];
+    int wrong = 0;
+    int victory = 0;
+
+    // Introduction
+    printf("Welcome to the Halloween Hangman game! Come play the game.\n");
     printf("What is your name? ");
-    if (fgets(name, sizeof name, stdin) == NULL) {
+
+    if (fgets(name, sizeof(name), stdin) == NULL) {
         name[0] = '\0';
     } else {
-        for (int i = 0; name[i]; ++i) {
+        for (int i = 0; name[i]; i++) {
             if (name[i] == '\n') { name[i] = '\0'; break; }
         }
     }
-    //Hannah-
-    //Make the hang man picture and wrong guesses.
 
-    if (wrong < 0) wrong = 0;
+    int word = rand() % (sizeof(words) / sizeof(words[0]));
+    char chosen_word[50];
+    strcpy(chosen_word, words[word]);
 
- 
+    for (int i = 0; i < (int)strlen(chosen_word); i++) {
+        board[i] = '_';
+    }
+    board[strlen(chosen_word)] = '\0';
+    wrongLetters[0] = '\0';
 
-    //arthur
-    srand(time(NULL));
-    char words[10][10] = ("boo", "black_cat", "ghost", "costume", "knight", "candy", "decay", "sweets", "screams", "pumpkin", "bats", "mummy", "snake_mummy", "Stab", "haunted" ,"Frankenstein", "werewolf", "dracula", "batman", "witch", "yeti", "scary", "screms");
-    int num = rand() % 23;
-    char chosen_word[] = words[num];// can anyone thcing else for the list that are not people in this grope or part of decay
+    while (wrong < 6 && victory == 0) {
+        char guess;
+        int correct = 0;
 
-    // Hannah
-    char board[20];
-char wrongLetters[20];
-int wrongCount = 0;
+        printf("\nWord: %s\n", board);
+        printf("Wrong guesses: %s\n", wrongLetters);
+        displayHangman(wrong);
 
-    for (int i = 0; i < strlen(chosen_word); i++) {
-    board[i] = '_';
-}
-board[strlen(chosen_word)] = '\0';
-wrongLetters[0] = '\0'; // starts empty
+        printf("Enter a letter: ");
+        if (scanf(" %c", &guess) != 1) {
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF) {}
+            continue;
+        }
 
-printf("\nWord: %s\n", board);
-printf("Wrong guesses: %s\n", wrongLetters);//
-    
-    if (victory == yes){//what is the condishion for this comdishional and where is the else statment I only see a if and an else if statment no else statment.
-        printf("congrats you guessed the word correctly!\n");
-    }else if (victory == no){
-        printf("you lost, you did not guess the right letters the word is %s.\n", chosen_word);
-    }else{
-        printf("the game is broken start over")
+        for (int i = 0; i < (int)strlen(chosen_word); i++) {
+            if (chosen_word[i] == guess) {
+                board[i] = guess;
+                correct = 1;
+            }
+        }
+
+        if (!correct) {
+            size_t len = strlen(wrongLetters);
+            wrongLetters[len] = guess;
+            wrongLetters[len + 1] = '\0';
+            wrong++;
+        }
+
+        if (strcmp(board, chosen_word) == 0) {
+            victory = 1;
+        }
     }
 
+    if (victory == 1) {
+        printf("\nGood job %s! You guessed the word right!\n", name);
+    } else {
+        printf("\nYou lost. The word was: %s.\n", chosen_word);
+    }
 
     return 0;
 }
-
-
-
